d858651f0caff0430ac362a64756d08a41b28379
Rohit <rohit.sharma2@hexaviewtech.com>
2022-05-15 22:25:00 +0530

    main() method

---
+++ src/test/java/com/Taks8/BuilderDesignPatternDemo/BuilderDesignPatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Taks8.BuilderDesignPatternDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderDesignPatternDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
